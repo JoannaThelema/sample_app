0e8dbe3f17a84f0404dc8e7c1ddc513473fb516c
Joanna <latekjo@gmail.com>
2017-01-18 15:56:30 +0100

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,7 +7,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index as admin including pagination and delete links" do
+  test "index (without nonactivated users) as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
@@ -15,9 +15,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
       end
     end
     assert_difference 'User.count', -1 do
@@ -30,4 +34,5 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
+
 end
